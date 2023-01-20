e7f030aea7ed863b746566c220e231a1de0b51cf
Charlie Conneely <charlie.conneely@genesys.com>
2023-01-20 15:34:37 +0000

    Upgraded gc terraform provider version

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,7 +2,7 @@ terraform {
     required_providers {
         genesyscloud = {
             source  = "mypurecloud/genesyscloud"
-            version = "1.9.0"
+            version = "1.10.0"
         }
     }
 }
